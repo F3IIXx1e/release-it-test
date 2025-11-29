dc23c1be978f828e0cee451b0fb9c10ef70d1a4d
Felix <duemarsgh@foxmail.com>
2025-11-29 13:18:24 +0800

    :sparkles: feat(root): 新增@release-it/conventional-changelog插件

--- .release-it.js
+++ .release-it.js
@@ -12,5 +12,14 @@ module.exports = {
     },
     github: {
         release: false
+    },
+    plugins: {
+        '@release-it/conventional-changelog': {
+            // 在当前目录下生成的文件名称
+            infile: 'CHANGELOG.md',
+            // CHANGELOG 顶部标题
+            header: '# 版本变更记录 - 由 release-it 自动生成',
+            preset: 'angular'
+        }
     }
 }
