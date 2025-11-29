61efebe2d7e990be1152762a0a609ccc49833122
Felix <duemarsgh@foxmail.com>
2025-11-29 13:01:05 +0800

    :sparkles: feat(root): 新增release-it基础配置项

--- .release-it.js
+++ .release-it.js
@@ -2,5 +2,15 @@
  * @type {import('release-it').Config}
  */
 module.exports = {
-    
+    git: {
+        tagName: 'v${version}',
+        commitMessage: ':bookmark: chore(release): release v${version} [skip ci]',
+        assets: ['CHANGELOG.md']
+    },
+    npm: {
+        publish: false
+    },
+    github: {
+        release: false
+    }
 }
